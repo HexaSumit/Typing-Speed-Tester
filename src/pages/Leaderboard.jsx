f19c64cc490c7e72c224fd3abe709862e8da9c7f
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-08-07 17:10:06 +0530

    Add leaderboard.jsx file

---
+++ src/pages/Leaderboard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+const leaderboardData = [
+    { rank: 1, username: "SpeedySumit", wpm: 145, accuracy: "98.2%" },
+    { rank: 2, username: "CodeMaster", wpm: 138, accuracy: "97.6%" },
+    { rank: 3, username: "TypeKing", wpm: 132, accuracy: "96.4%" },
+    { rank: 4, username: "ReactRider", wpm: 129, accuracy: "97.1%" },
+    { rank: 5, username: "FastFingers", wpm: 125, accuracy: "95.8%" },
+    { rank: 6, username: "TypoNinja", wpm: 119, accuracy: "96.7%" },
+    { rank: 7, username: "SpeedWriter", wpm: 115, accuracy: "94.3%" },
+    { rank: 8, username: "AsyncAce", wpm: 112, accuracy: "95.2%" },
+    { rank: 9, username: "BugHunter", wpm: 110, accuracy: "93.5%" },
+    { rank: 10, username: "LoopLord", wpm: 106, accuracy: "92.7%" },
+    { rank: 11, username: "ConsoleQueen", wpm: 101, accuracy: "91.6%" },
+    { rank: 12, username: "DevDynamo", wpm: 97, accuracy: "93.0%" },
+    { rank: 13, username: "KeyStroker", wpm: 95, accuracy: "90.8%" },
+    { rank: 14, username: "CharCoder", wpm: 91, accuracy: "89.4%" },
+    { rank: 15, username: "PixelPusher", wpm: 89, accuracy: "88.1%" },
+];
+const Leaderboard = () => {
+    return (
+        <div>Leaderboard</div>
+    )
+}
+
+export default Leaderboard
